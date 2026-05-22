3eb722fb709a3ba549548d147a4713ff59c4c5bb
Caique Mattner Matos <caique.mattner@gmail.com>
2026-05-22 20:21:58 -0300

    app created

--- project/urls.py
+++ project/urls.py
@@ -20,11 +20,21 @@ from django.http import HttpResponse
 from django.urls import path
 
 
-def my_view(request):
-    return HttpResponse("Uma linda string")
+def home(request):
+    return HttpResponse("HOME")
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sobre/", my_view),
+    path("", home),  # Home
+    path("sobre/", sobre),  # /sobre/
+    path("contato/", contato),  # /contato/
 ]
