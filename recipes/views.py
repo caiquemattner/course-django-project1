af70dce4b746d8aadd960cf018ffaa2a08311fc9
Caique Mattner Matos <caique.mattner@gmail.com>
2026-05-23 20:54:20 -0300

    templates created

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("HOME 1")
+    return render(request, "recipes/home.html")
 
 
 def sobre(request):
