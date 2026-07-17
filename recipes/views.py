fd5a9d52d6b4b829fcc703e8e6d1cc1b6d0089a8
Caique Mattner Matos <caique.mattner@gmail.com>
2026-07-17 20:27:31 -0300

    filtering is_published

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by("-id")
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
     return render(
         request,
         "recipes/pages/home.html",
@@ -17,10 +17,12 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by("-id")
     return render(
         request,
-        "recipes/pages/home.html",
+        "recipes/pages/category.html",
         context={
             "recipes": recipes,
         },
