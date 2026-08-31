54957231d71809d0702d06c9e1d4ce7fbb94ef21
Caique Mattner Matos <caique.mattner@gmail.com>
2026-08-31 20:19:03 -0300

    updating views

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,10 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by("-id")
+
     return render(
         request,
         "recipes/pages/home.html",
