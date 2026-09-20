d18d0df2c605aa896ffc0dce211fc165db38df58
Caique Mattner Matos <caique.mattner@gmail.com>
2026-09-20 19:21:02 -0300

    asserts

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipeURLsTests(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
